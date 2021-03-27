3cba12f466c549d22875f530c73eb519874f21b8
Surajit Mukhopadhyay <ADMSMU216@CRB.APMOLLER.NET>
2021-03-27 14:40:06 +0530

    Added Redis

--- src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -1,11 +1,20 @@
 package com.example.uploadingfiles;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+import com.example.uploadingfiles.storage.Receiver;
 import com.example.uploadingfiles.storage.StorageProperties;
 import com.example.uploadingfiles.storage.StorageService;
 
@@ -13,8 +22,22 @@ import com.example.uploadingfiles.storage.StorageService;
 @EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = 
 		SpringApplication.run(UploadingFilesApplication.class, args);
+		
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		Receiver receiver = ctx.getBean(Receiver.class);
+
+		while (receiver.getCount() == 0) {
+
+			LOGGER.info("Sending message...");
+			template.convertAndSend("chat", "Hello from Redis!");
+			Thread.sleep(500L);
+		}
+
+		System.exit(0);
+		
 	}
 
 	@Bean
@@ -23,5 +46,33 @@ public class UploadingFilesApplication {
 			//storageService.deleteAll();
 			storageService.init();
 		};
+	}
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UploadingFilesApplication.class);
+
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	@Bean
+	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
 	}
 }
