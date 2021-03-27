7b7bb95b1567587f1aa4b4046838fc6eefe6a0a4
Surajit Mukhopadhyay <ADMSMU216@CRB.APMOLLER.NET>
2021-03-27 15:31:52 +0530

    updated

--- src/main/java/com/example/uploadingfiles/storage/Receiver.java
+++ src/main/java/com/example/uploadingfiles/storage/Receiver.java
@@ -4,9 +4,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public class Receiver {
     private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
